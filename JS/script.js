baa38675e901fb9a6fa458b9c3e4feeb495e73ff
Moyshin94 <96650325+Moyshin94@users.noreply.github.com>
2023-05-29 09:41:34 +0300

    change mistake

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const x = prompt("Write a wich is hour, i will conwert it into seconds");
 
 console.log(x);
 
-const y = x * 60;
+const y = x * 3600;
 
 console.log(y);
 alert(`in ${x} hours ${y} seconds`);
