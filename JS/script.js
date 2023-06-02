afd41971565c3fdcd998daa940bf674579b2020a
Moyshin94 <96650325+Moyshin94@users.noreply.github.com>
2023-06-02 21:26:46 +0300

    tru  to do if else

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,111 @@
-const x = prompt("Write a hour, i will conwert it into seconds");
+// HM 2
+// const x = prompt('Write a hour, i will conwert it into seconds');
 
-console.log(x);
+// console.log(x);
 
-const y = x * 3600;
+// const y = x * 3600;
 
-console.log(y);
-alert(`in ${x} hours ${y} seconds`);
+// console.log(y);
+// alert(`in ${x} hours ${y} seconds`);
+
+// console.log();
+// END HM 2
+
+// // form lesson
+// const userAge = null;
+// const obj = {
+//   // age: userAge !== undefined &§ userAge !as null ? userAge : -1, age: userAge ?? -1,
+//   age: userAge ?? -1,
+// };
+// console.log(obj.age);
+
+// HM 3
+
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr);
+
+// if (numOrStr === null) {
+//   console.log('ви відмінили');
+// } else if (numOrStr.trim() === '') {
+//   console.log('Empty String');
+// } else if (isNaN(+numOrStr)) {
+//   console.log(' number is Ba_NaN');
+// } else {
+//   console.log('OK!');
+// }
+
+// let numOrStr = prompt('Input number or string');
+// console.log(numOrStr);
+
+// switch (true) {
+//   case numOrStr === null:
+//     console.log('Ви відмінили');
+//     break;
+//   case numOrStr.trim() === '':
+//     console.log('Empty String');
+//     break;
+//   case isNaN(+numOrStr):
+//     console.log('Number is NaN');
+//     break;
+//   default:
+//     console.log('OK!');
+//     break;
+// }
+// END HM 3
+
+// HM 4
+// Prompt for the user's year of birth
+let yearOfBirth = prompt('Enter your year of birth');
+if (yearOfBirth === null) {
+  alert("It's a pity that you did not want to enter your year of birth.");
+} else {
+  // Prompt for the city the user lives in
+  let city = prompt('Enter the city you live in');
+  if (city === null) {
+    alert("It's a pity that you did not want to enter your city.");
+  } else {
+    // Prompt for the user's favorite sport
+    let favoriteSport = prompt('Enter your favorite sport');
+    if (favoriteSport === null) {
+      alert("It's a pity that you did not want to enter your favorite sport.");
+    } else {
+      // Calculate the user's age
+      let currentYear = new Date().getFullYear();
+      let age = currentYear - parseInt(yearOfBirth);
+
+      // Construct the message based on the inputs
+      let message = 'Your age: ' + age;
+      if (city === 'Kyiv' || city === 'Washington' || city === 'London') {
+        message += 'You live in the capital of ' + city;
+      } else {
+        message += 'You live in the city: ' + city;
+      }
+
+      let champions = {
+        Football: 'Lionel Messi',
+        Basketball: 'LeBron James',
+        Tennis: 'Serena Williams',
+      };
+      if (favoriteSport in champions) {
+        message += 'Cool! Do you want to become ';
+      }
+
+      // Display the information in an alert
+      alert(message);
+    }
+  }
+}
+// END HM 4
+
+// HM 5
+// const firstName = prompt('What is your name?');
+
+// console.log(firstName);
+
+// alert(`Hello, ${firstName}! How are you?`);
+
+// END HM 5
+
+// HM 6
+
+// END HM 6
---
+++ JS/script2.js
@@ -0,0 +1,5 @@
+// let yearOfBirth = prompt('Write your birth year');
+// let liveCity = prompt('Write a city you live in');
+// let favSport = prompt('Write your favorite sport');
+
+// alert(`You are ${yearOfBirth} years old.`);
