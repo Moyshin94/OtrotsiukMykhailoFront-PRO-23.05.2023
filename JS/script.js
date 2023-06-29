a691b7dd365e8bfdd42b1236232e10d6ea9f9a52
Moyshin94 <96650325+Moyshin94@users.noreply.github.com>
2023-06-29 18:38:35 +0300

    push

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,14 @@
-// HM 2
-// const x = prompt('Write a hour, i will conwert it into seconds');
+HM 2
+const x = prompt('Write a hour, i will conwert it into seconds');
 
-// console.log(x);
+console.log(x);
 
-// const y = x * 3600;
+const y = x * 3600;
 
-// console.log(y);
-// alert(`in ${x} hours ${y} seconds`);
+console.log(y);
+alert(`in ${x} hours ${y} seconds`);
 
-// console.log();
+console.log();
 // END HM 2
 
 // // form lesson
@@ -55,47 +55,47 @@
 
 // HM 4
 // Prompt for the user's year of birth
-let yearOfBirth = prompt('Enter your year of birth');
-if (yearOfBirth === null) {
-  alert("It's a pity that you did not want to enter your year of birth.");
-} else {
-  // Prompt for the city the user lives in
-  let city = prompt('Enter the city you live in');
-  if (city === null) {
-    alert("It's a pity that you did not want to enter your city.");
-  } else {
-    // Prompt for the user's favorite sport
-    let favoriteSport = prompt('Enter your favorite sport');
-    if (favoriteSport === null) {
-      alert("It's a pity that you did not want to enter your favorite sport.");
-    } else {
-      // Calculate the user's age
-      let currentYear = new Date().getFullYear();
-      let age = currentYear - yearOfBirth;
+// let yearOfBirth = prompt('Enter your year of birth');
+// if (yearOfBirth === null) {
+//   alert("It's a pity that you did not want to enter your year of birth.");
+// } else {
+//   // Prompt for the city the user lives in
+//   let city = prompt('Enter the city you live in');
+//   if (city === null) {
+//     alert("It's a pity that you did not want to enter your city.");
+//   } else {
+//     // Prompt for the user's favorite sport
+//     let favoriteSport = prompt('Enter your favorite sport');
+//     if (favoriteSport === null) {
+//       alert("It's a pity that you did not want to enter your favorite sport.");
+//     } else {
+//       // Calculate the user's age
+//       let currentYear = new Date().getFullYear();
+//       let age = currentYear - yearOfBirth;
 
-      // Construct the message based on the inputs
-      let message = 'Your age: ' + age;
-      if (city === 'Kyiv' || city === 'Washington' || city === 'London') {
-        message += 'You live in the capital of ' + city;
-      } else {
-        message += 'You live in the city: ' + city;
-      }
+//       // Construct the message based on the inputs
+//       let message = 'Your age: ' + age;
+//       if (city === 'Kyiv' || city === 'Washington' || city === 'London') {
+//         message += 'You live in the capital of ' + city;
+//       } else {
+//         message += 'You live in the city: ' + city;
+//       }
 
-      let champions = {
-        Football: 'Lionel Messi',
-        Basketball: 'LeBron James',
-        Tennis: 'Serena Williams',
-      };
-      if (favoriteSport in champions) {
-        message +=
-          'Cool! Do you want to become ' + champions[favoriteSport] + '?';
-      }
+//       let champions = {
+//         Football: 'Lionel Messi',
+//         Basketball: 'LeBron James',
+//         Tennis: 'Serena Williams',
+//       };
+//       if (favoriteSport in champions) {
+//         message +=
+//           'Cool! Do you want to become ' + champions[favoriteSport] + '?';
+//       }
 
-      // Display the information in an alert
-      alert(message);
-    }
-  }
-}
+//       // Display the information in an alert
+//       alert(message);
+//     }
+//   }
+// }
 // END HM 4
 
 // HM 5
